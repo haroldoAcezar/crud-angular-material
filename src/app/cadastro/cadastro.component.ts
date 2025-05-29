9f959b562b4397868ef0872521607cd59a3c4abb
HaroldoCezar <haroldocezar@pageware.com.br>
2025-05-28 21:36:56 -0300

    Criando form

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -4,14 +4,17 @@ import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-cadastro',
   imports: [FlexLayoutModule, 
     MatCardModule, 
     FormsModule, 
     MatFormFieldModule,
-    MatInputModule],
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
